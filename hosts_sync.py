142151a9d9ce0925d4cb5323bc817a59ac43e979
045200 <wsir9527@gmail.com>
2025-04-23 19:54:59 +0800

    更新 hosts_sync.py

--- hosts_sync.py
+++ hosts_sync.py
@@ -12,12 +12,7 @@ RULE_DIR = os.path.join(DATA_DIR, "rules")
 # 下载源配置
 HOSTS_SOURCES = [
  "https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all",
-
-"https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts",
  "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-hosts.txt",
-    "https://raw.githubusercontent.com/ineo6/hosts/refs/heads/master/hosts",
-
-"https://raw.githubusercontent.com/maxiaof/github-hosts/refs/heads/master/hosts",
 
 "https://raw.hellogithub.com/hosts"
 
